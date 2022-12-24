eb2f2cd1ee91d24122a6169c3befa1b05a67670f
sandeephooda <sonu.hooda@gmail.com>
2022-12-24 08:54:14 -0500

    Spring @Autowire bean resolution

--- src/main/java/com/sandeep/validation/EndPoint.java
+++ src/main/java/com/sandeep/validation/EndPoint.java
@@ -2,21 +2,45 @@ package com.sandeep.validation;
 
 
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sandeep.validation.vo.Address;
 import com.sandeep.validation.vo.Person;
 
 @RestController
 public class EndPoint {
 	
+	
+	@Autowired
+	Address myAddress;
+		
+	@Autowired
+	Person get10Year;
+	
+	@Autowired
+	@Qualifier("get20Year")
+	Person aPerson;
+	
+	
+	@Autowired
+	List<Person> listOfPersons;
+	
 	@GetMapping("/sayhello")
 	public String sayHello() {
+		System.out.println(" myAddress "+myAddress.getStreetAddress());
+		System.out.println(" a 10 year "+get10Year.getAge());
+		System.out.println(" a 20 year old  "+aPerson.getAge());
+		System.out.println(" person list size : "+listOfPersons.size());
 		return "Hello world";
 	}
 	
--- src/main/java/com/sandeep/validation/ValidationApplication.java
+++ src/main/java/com/sandeep/validation/ValidationApplication.java
@@ -2,6 +2,10 @@ package com.sandeep.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sandeep.validation.vo.Address;
+import com.sandeep.validation.vo.Person;
 
 @SpringBootApplication
 public class ValidationApplication {
@@ -9,5 +13,29 @@ public class ValidationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ValidationApplication.class, args);
 	}
+	
+	@Bean
+	public Address getAddress1() {
+		Address a = new Address();
+		a.setStreetAddress(" add 1");
+		return a;
+	}
 
+	@Bean
+	public Person get10Year() {
+		Person p = new Person();
+		p.setAge(10);
+		return p;
+	}
+	
+	@Bean
+	public Person get20Year() {
+		Person p = new Person();
+		p.setAge(20);
+		return p;
+	}
+	
+
+	
+	
 }
